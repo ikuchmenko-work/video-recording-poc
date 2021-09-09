4657383c9e34aa95c6f0be388392791ae8059970
Sabjeslav <s4tenko@gmail.com>
2021-09-09 10:44:51 +0300

    feat: added bottom navigation with camera and home screens

---
+++ screens/CameraScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const CameraScreen = () => {
+    return (
+        <View>
+            <Text>Camera screen</Text>
+        </View>
+    );
+};
+
+export default CameraScreen;
---
+++ screens/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View style={{display: "flex", flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text style={{color: "#000"}}>Home page</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
