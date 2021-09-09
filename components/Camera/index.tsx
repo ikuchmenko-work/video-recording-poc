d22a810c4bf82582126e6773a48438228b42823c
Sabjeslav <s4tenko@gmail.com>
2021-09-09 12:08:28 +0300

    feat: added camera image displaying

--- components/Camera/index.tsx
+++ components/Camera/index.tsx
@@ -1,18 +1,96 @@
-import React from 'react';
-import {Button, Text, View} from "react-native";
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {NavigationProp} from "@react-navigation/native";
+import {Camera} from "expo-camera";
+import {CameraType} from "expo-camera/build/Camera.types";
 
 type Props = {
     navigation: NavigationProp<any>
 }
 
 const CameraComponent = ({navigation}: Props) => {
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+    const [type, setType] = useState<CameraType>(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (!hasPermission) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
-        <View>
-            <Text>Record a video!</Text>
-            <Button title={"Watch video"} onPress={()=>{navigation.navigate("Player")}}/>
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <Text style={styles.text}> Record video </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{...styles.button, ...styles.flipBtn}}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <Text style={styles.text}> Flip </Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+            {/*<Button title={"Watch video"} onPress={()=>{navigation.navigate("Player")}}/>*/}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        justifyContent: "center",
+        margin: 20,
+    },
+    button: {
+        flex: 0.5,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 5,
+        backgroundColor: "black"
+    },
+    flipBtn: {
+        flex: 0.3,
+        backgroundColor: "transparent"
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
+});
 
 export default CameraComponent;
