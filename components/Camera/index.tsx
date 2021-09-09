8825c4e026fd2d6d61dfb4df5443fb12e12ee5a4
Sabjeslav <s4tenko@gmail.com>
2021-09-09 17:31:21 +0300

    feat: Added video recording and video player

--- components/Camera/index.tsx
+++ components/Camera/index.tsx
@@ -2,73 +2,83 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {NavigationProp, useIsFocused} from "@react-navigation/native";
 import {Camera} from "expo-camera";
-import {CameraType} from "expo-camera/build/Camera.types";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { FontAwesome } from '@expo/vector-icons';
 
 type Props = {
     navigation: NavigationProp<any>
 }
 
 const CameraComponent = ({navigation}: Props) => {
-    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-    const [type, setType] = useState<CameraType>(Camera.Constants.Type.back);
+    const [hasAudioPermission, setHasAudioPermission] = useState<boolean | null>(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
+    const [camera, setCamera] = useState<any>(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
     const isFocused = useIsFocused();
+
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-        return (()=> {
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
 
-        })
+            const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+            setHasAudioPermission(audioStatus.status === 'granted');
+        })();
     }, []);
 
-    if (hasPermission === null) {
-        return <View />;
-    }
-    if (!hasPermission) {
-        return <Text>No access to camera</Text>;
+    const takeVideo = async () => {
+        if(camera){
+            const data = await camera.recordAsync({
+                maxDuration:3
+            })
+            navigation.navigate('Player', {
+                uri: data.uri
+            })
+        }
     }
 
+    if (hasCameraPermission === null || hasAudioPermission === null ) {
+        return <View />;
+    }
+    if (!hasCameraPermission || !hasAudioPermission) {
+        return <Text>No access to camera</Text>;
+    }
     return (
-        <View style={styles.container}>
-            {isFocused && <Camera style={styles.camera} type={type}>
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                  style={styles.button}
-                  onPress={() => {
-                      setType(
-                          type === Camera.Constants.Type.back
-                              ? Camera.Constants.Type.front
-                              : Camera.Constants.Type.back
-                      );
-                  }}>
-                  <Text style={styles.text}> Record video </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{...styles.button, ...styles.flipBtn}}
-                  onPress={() => {
-                      setType(
-                          type === Camera.Constants.Type.back
-                              ? Camera.Constants.Type.front
-                              : Camera.Constants.Type.back
-                      );
-                  }}>
-                  <Text style={styles.text}> Flip </Text>
-                </TouchableOpacity>
-              </View>
-            </Camera>}
-            {/*<Button title={"Watch video"} onPress={()=>{navigation.navigate("Player")}}/>*/}
-        </View>
+            <View style={styles.cameraContainer}>
+                {isFocused &&
+                    <Camera
+                      ref={(ref) => setCamera(ref)}
+                      style={styles.fixedRatio}
+                      type={type}
+                      ratio={'4:3'} >
+                      <View style={styles.buttonContainer}>
+
+                        <TouchableOpacity onPress={() => takeVideo()} style={styles.button}>
+                          <FontAwesome name="circle" size={60} color="white" />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                          style={styles.button}
+                          onPress={() => {
+                              setType(
+                                  type === Camera.Constants.Type.back
+                                      ? Camera.Constants.Type.front
+                                      : Camera.Constants.Type.back
+                              );
+                          }}>
+                          <Ionicons name={"camera-reverse"} color={"white"} size={40} />
+
+                        </TouchableOpacity>
+                      </View>
+                    </Camera>
+                }
+            </View>
+
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    camera: {
-        flex: 1,
-    },
     buttonContainer: {
         flex: 1,
         backgroundColor: 'transparent',
@@ -77,23 +87,38 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     button: {
-        flex: 0.5,
         alignSelf: 'flex-end',
-        alignItems: 'center',
         padding: 10,
-        borderWidth: 1,
-        borderColor: "black",
         borderRadius: 5,
-        backgroundColor: "black"
     },
     flipBtn: {
-        flex: 0.3,
+        flex: 1,
         backgroundColor: "transparent"
     },
     text: {
         fontSize: 18,
         color: 'white',
     },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    fixedRatio:{
+        flex: 1,
+    },
+    video: {
+        alignSelf: 'center',
+        width: 350,
+        height: 220,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    recordBtn: {
+        alignSelf: "center"
+    }
 });
 
 export default CameraComponent;
--- components/Player/index.tsx
+++ components/Player/index.tsx
@@ -1,19 +1,77 @@
-import React from 'react';
-import {Button, Text, View} from "react-native";
-import {NavigationProp} from "@react-navigation/native";
+import React, {useRef} from 'react';
+import {Button, StyleSheet, View} from "react-native";
+import {Video} from "expo-av";
 
-type Props = {
-    navigation: NavigationProp<any>
-}
-
-const Player = ({navigation}: Props) => {
+const Player = ({route, navigation}: any) => {
+    const video = useRef(null);
+    const data = route.params;
     return (
-        <View>
-            <Text>Player screen</Text>
+        <View style={styles.container}>
+            <Video
+                ref={video}
+                style={styles.video}
+                source={{
+                    uri: data.uri,
+                }}
+                useNativeControls
+                isLooping={true}
+                resizeMode="contain"
+            />
             <Button title={"Home"} onPress={()=>{navigation.navigate("Home")}} />
             <Button title={"Go back"} onPress={()=>{navigation.goBack()}} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        justifyContent: "center",
+        margin: 20,
+    },
+    button: {
+        flex: 0.5,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 5,
+        backgroundColor: "black",
+        marginHorizontal: 10
+    },
+    flipBtn: {
+        flex: 0.3,
+        backgroundColor: "transparent"
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio:{
+        flex: 1,
+        // aspectRatio: 1
+    },
+    video: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default Player;
