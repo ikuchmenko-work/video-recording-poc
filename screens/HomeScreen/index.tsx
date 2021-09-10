fddec74a6c938b3d052efa7a75e2243f962b3e0e
Sabjeslav <s4tenko@gmail.com>
2021-09-10 12:02:36 +0300

    refactor: Removed inline styles in home screen

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import {Text, View} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
-    return (
-        <View style={{display: "flex", flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text style={{color: "#000"}}>Home page</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home page</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#000",
+  },
+});
 
 export default HomeScreen;
